62f855c14c34d8ae1360b69ddb9680c3ed28fe87
Rakesh Rajbanshi <merakerr@gmail.com>
2021-08-07 16:41:53 +0530

    node to root path

---
+++ tree/genericTree/nodeToRootPath.java
@@ -0,0 +1,91 @@
+/*
+           1
+        /  |  \
+       2   3   4
+    /      |    \
+  5        6     8
+
+  node to root path for 6 is ---->> 6-->3-->1
+  node to root path for 8 is ---->> 8-->4-->1
+
+*/
+    
+    import java.io.*;
+    import java.util.*;
+    
+    public class nodeToRootPath {
+      private static class Node {
+        int data;
+        ArrayList<Node> children = new ArrayList<>();
+      }
+    
+      public static void display(Node node) {
+        String str = node.data + " -> ";
+        for (Node child : node.children) {
+          str += child.data + ", ";
+        }
+        str += ".";
+        System.out.println(str);
+    
+        for (Node child : node.children) {
+          display(child);
+        }
+      }
+    
+      public static Node construct(int[] arr) {
+        Node root = null;
+    
+        Stack<Node> st = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+          if (arr[i] == -1) {
+            st.pop();
+          } else {
+            Node t = new Node();
+            t.data = arr[i];
+    
+            if (st.size() > 0) {
+              st.peek().children.add(t);
+            } else {
+              root = t;
+            }
+    
+            st.push(t);
+          }
+        }
+    
+        return root;
+      }
+    
+    //code written here *****************************************************
+     public static ArrayList<Integer> nodeToRootPath(Node node, int data){
+        if(node.data == data){
+            ArrayList<Integer> bres = new ArrayList<>();
+            bres.add(node.data);
+            return bres;
+        }
+        for(Node child : node.children){
+            ArrayList<Integer> rres = nodeToRootPath(child, data);
+            if(rres.size() > 0){  //means items is found 
+                rres.add(node.data);
+                return rres;
+            }
+        }
+     }
+      public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        String[] values = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+          arr[i] = Integer.parseInt(values[i]);
+        }
+    
+        int data = Integer.parseInt(br.readLine());
+    
+        Node root = construct(arr);
+        ArrayList<Integer> path = nodeToRootPath(root, data);
+        System.out.println(path);
+        // display(root);
+      }
+    
+    }
