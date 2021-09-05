50443e8f14c407cca48c9d818fe566cd1182e3eb
Rakesh Rajbanshi <merakerr@gmail.com>
2021-09-05 22:09:38 +0530

    maximum swap

--- arrayString2/maximumSwap.java
+++ arrayString2/maximumSwap.java
@@ -1,5 +1,7 @@
 
-    /*
+  //leetcode 670
+  
+  /*
 You are given an integer num. You can swap two digits at most once to get the 
 maximum valued number.
 Return the maximum valued number you can get.
