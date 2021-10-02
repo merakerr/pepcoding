fb7df95872f43150d28f44c79c253efec17b8a69
Rakesh Rajbanshi <merakerr@gmail.com>
2021-10-02 19:55:45 +0530

    kosaraju

---
+++ graph_2/kosaraju.java
@@ -0,0 +1,103 @@
+/*
+You are given a graph with N nodes and M directed edges. Find the number of
+ Strongly connected components in the graph.
+Input Format
+First line contains two space separated integers,N and M. Then M lines follow,
+ each line has 2 space separated integers ai and bi.
+ Input:
+5 6
+1 4
+1 3
+2 4
+3 4
+4 5
+5 1
+Sample Output
+2
+*/
+
+    import java.util.*;
+
+    public class kosaraju {
+    
+        private static void dfsStack(ArrayList<ArrayList<Integer>> graph,
+            int src, boolean[] vis, Stack<Integer> st) {
+            
+            vis[src] = true;
+            for(int nbr : graph.get(src)) {
+                if(vis[nbr] == false) {
+                    dfsStack(graph, nbr, vis, st);
+                }
+            }
+            st.push(src);
+        }
+    
+        private static void dfsCount(ArrayList<ArrayList<Integer>> graph,
+            int src, boolean[] vis) {
+            
+            vis[src] = true;
+            for(int nbr : graph.get(src)) {
+                if(vis[nbr] == false) {
+                    dfsCount(graph, nbr, vis);
+                }
+            }
+        }
+    
+        private static int stronglyConnectedComp(ArrayList<ArrayList<Integer>> graph) {
+            // 1. iterate on vertex and add vertex in stack while backtracking
+            Stack<Integer> st = new Stack<>();
+            boolean[] vis = new boolean[graph.size()];
+            for(int v = 0; v < graph.size(); v++) {
+                if(vis[v] == false) {
+                    dfsStack(graph, v, vis, st);
+                }
+            }
+    
+            // 2. reverse all edge
+            ArrayList<ArrayList<Integer>> revGraph = new ArrayList<>();
+            for(int i = 0; i < graph.size(); i++) {
+                revGraph.add(new ArrayList<>());
+            } 
+                // original graph, v --> u
+            for(int v = 0; v < graph.size(); v++) {
+                for(int u : graph.get(v)) {
+                    revGraph.get(u).add(v);
+                }
+            }
+    
+            // 3. iterate on vertex in order of stack and count number of calls
+            int count = 0;
+            vis = new boolean[graph.size()];
+            while(st.size() > 0) {
+                int v = st.pop();
+                if(vis[v] == false) {
+                    count++;
+                    dfsCount(revGraph, v, vis);
+                }
+            }
+            return count;
+        }
+    
+        public static void main(String[] args) {
+            Scanner scn = new Scanner(System.in);
+            int n = scn.nextInt();
+            int m = scn.nextInt();
+    
+            ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+    
+            for(int i = 0; i < n; i++) {
+                graph.add(new ArrayList<>());
+            }
+    
+            for(int i = 0; i < m; i++) {
+                int v1 = scn.nextInt() - 1;
+                int v2 = scn.nextInt() - 1;
+    
+                // add directed edge 
+                graph.get(v1).add(v2);
+            }
+    
+            int count = stronglyConnectedComp(graph);
+            System.out.println(count);
+        }
+    }
