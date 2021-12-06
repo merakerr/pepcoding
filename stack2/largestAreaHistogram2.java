15ff2f9a6793f4817305d98b75f1afc487ec728f
Rakesh Rajbanshi <merakerr@gmail.com>
2021-12-06 20:15:04 +0530

    largest area histogram

--- stack2/largestAreaHistogram2.java
+++ stack2/largestAreaHistogram2.java
@@ -12,7 +12,46 @@ Note. This question is same as Largest Area Histogram just the constrains are hi
     
     public class largestAreaHistogram2 {
         public static int largestRectangleArea(int[] heights) {
-            return 0;
+            int[] nsr = nsr_r(heights);
+            int[] nsl = nsl_l(heights);
+            int res = 0;
+            for(int i = 0; i < heights.length; i++){
+                int weight = nsr[i] - nsl[i] - 1;
+                int area = weight * heights[i];
+                res = Math.max(res, area);
+            }
+            return res;
+        }
+
+        public static int[] nsr_r(int[] arr){
+          Stack<Integer> st = new Stack<>();
+          int[] res = new int[arr.length];
+
+          for(int i = 0; i < arr.length; i++){
+              while(st.size() > 0 && arr[st.peek()] > arr[i]){
+                  res[st.pop()] = i;
+              }
+              st.push(i);
+          }
+
+          while(st.size() > 0){
+              res[st.pop()] = arr.length;
+          }
+          return res;
+        }
+        public static int[] nsl_l(int[] arr){
+           Stack<Integer> st = new Stack<>();
+           int[] res = new int[arr.length];
+           for(int i = arr.length -1; i >= 0 ; i--){
+               while(st.size() > 0 && arr[st.peek()] > arr[i]){
+                   res[st.pop()] = i;
+               }
+               st.push(i);
+           }
+           while(st.size() > 0){
+               res[st.pop()] = -1;
+           }
+           return res;
         }
     
         public static void main(String[] args) throws Exception {
