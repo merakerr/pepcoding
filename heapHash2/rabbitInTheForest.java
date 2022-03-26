fde5219ea21be9292d4967c7f1068e1393174173
Rakesh Rajbanshi <merakerr@gmail.com>
2022-03-26 18:42:00 +0530

    rabbit in the forest

---
+++ heapHash2/rabbitInTheForest.java
@@ -0,0 +1,65 @@
+/*
+leetcode 781
+
+
+There is a forest with an unknown number of rabbits. We asked n rabbits "How many rabbits have the same color as you?" and collected the answers in an integer array answers where answers[i] is the answer of the ith rabbit.
+
+Given the array answers, return the minimum number of rabbits that could be in the forest.
+
+ 
+
+Example 1:
+
+Input: answers = [1,1,2]
+Output: 5
+Explanation:
+The two rabbits that answered "1" could both be the same color, say red.
+The rabbit that answered "2" can't be red or the answers would be inconsistent.
+Say the rabbit that answered "2" was blue.
+Then there should be 2 other blue rabbits in the forest that didn't answer into the array.
+The smallest possible number of rabbits in the forest is therefore 5: 3 that answered plus 2 that didn't.
+Example 2:
+
+Input: answers = [10,10,10]
+Output: 11
+ 
+*/
+    
+    import java.util.*;
+
+    public class rabbitInTheForest {
+    
+        public static int solution(int[] arr) {
+            HashMap<Integer, Integer> map = new HashMap<>();
+            
+            for(int val : arr){
+                map.put(val, map.getOrDefault(val, 0) + 1);
+            }
+            
+            int res = 0;
+            
+            //traverse through keySet()
+            for(int key : map.keySet()){
+                int groupSize = key + 1;
+                int reportees = map.get(key);
+                
+                int numOfGroupRequired = (int)Math.ceil(reportees * 1.0 / groupSize * 1.0);
+                    res += groupSize * numOfGroupRequired;
+                    
+                
+            }
+            return res;
+        }
+        
+        public static void main(String[] args) {
+            Scanner scn = new Scanner(System.in);
+            int n = scn.nextInt();
+            int[] arr = new int[n];
+            for(int i = 0 ; i < n; i++){
+                arr[i] = scn.nextInt();
+            }
+            System.out.println(solution(arr));
+        }
+    
+    }
+    
