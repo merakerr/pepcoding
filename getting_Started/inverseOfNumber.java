ddcf955609b9ff4fa462f258c57254f954c4626c
Rakesh Rajbanshi <merakerr@gmail.com>
2022-04-16 08:18:50 +0530

    inverse of a number

--- getting_Started/inverseOfNumber.java
+++ getting_Started/inverseOfNumber.java
@@ -7,14 +7,13 @@ public class inverseOfNumber {
    public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
-   int i = 1;
-   int ans = 0;
-   while(n!=0){
-       int r = n%10;
-       n = n/10;
-       ans = ans+i*(int)Math.pow(10, r-1);
-       i++;
+   int sum = 0;
+   while(n != 0){
+      int remainder = n % 10;
+      sum = sum * 10 + remainder;
+      n = n / 10;
    }
-   System.out.println(ans);
+   System.out.println(sum);
+
    }
 }
